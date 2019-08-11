24b55e99c24d69647ec2e4f658d25355d359db31
Tilak Raj <tilakraj1456@gmail.com>
2019-08-11 21:13:41 +0530

    updated github profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,12 +59,12 @@ jQuery(document).ready(function($) {
         }
     );
     
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
+    /* Github Calendar - https://github.com/CodeTheCloud/github-calendar */
+    new GitHubCalendar("#github-graph", "CodeTheCloud");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "CodeTheCloud", selector: "#ghfeed" });
 
 
 });
